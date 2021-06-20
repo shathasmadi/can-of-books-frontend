22e694092bed6a052a8bb5a2dacf6c99079fc148
Aseel-Dweedar <archaseel.1992@gmail.com>
2021-06-20 15:05:21 +0300

    add app

--- src/header.js
+++ src/header.js
@@ -1,19 +1,19 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Navbar from 'react-bootstrap/Navbar';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "react-bootstrap/Navbar";
 import { Link } from "react-router-dom";
-import './header.css';
+import "./header.css";
 
 class Header extends React.Component {
   render() {
-    return(
+    return (
       <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
         <Navbar.Brand>My Favorite Books</Navbar.Brand>
-          <Link to="/">Home</Link>
-          <Link to="/profile">Profile</Link>
-          {/* TODO: if the user is logged in, render the `LogoutButton` - if the user is logged out, render the `LoginButton` */}
+        <Link to="/">Home</Link>
+        <Link to="/profile">Profile</Link>
+        {/* TODO: if the user is logged in, render the `LogoutButton` - if the user is logged out, render the `LoginButton` */}
       </Navbar>
-    )
+    );
   }
 }
 
