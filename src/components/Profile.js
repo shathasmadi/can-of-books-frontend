09924c541434e3c8e12eed2b48ab967f692c3e69
Aseel-Dweedar <archaseel.1992@gmail.com>
2021-06-20 16:39:36 +0300

    fix errors

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -3,12 +3,13 @@ import { withAuth0 } from "@auth0/auth0-react";
 
 export class Profile extends Component {
   render() {
+    const { isAuthenticated } = this.props.auth0;
     return (
       isAuthenticated && (
         <div>
           <img src={this.props.auth0.user.picture} alt={this.props.auth0.user.name} />
-          <h2>{this.props.auth0.user.name}</h2>
-          <p>{this.props.auth0.user.email}</p>
+          <h2> {this.props.auth0.user.name} </h2>
+          <p> {this.props.auth0.user.email} </p>
         </div>
       )
     );
