b40767983b32a55877cbf37c74c5b251cf22738a
root <root@DESKTOP-H8Q20A4.localdomain>
2021-06-22 16:29:59 +0300

    add a frontend lab13

--- src/components/BestBooks.js
+++ src/components/BestBooks.js
@@ -1,44 +1,119 @@
-import React from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
-import { withAuth0 } from "@auth0/auth0-react";
-import axios from "axios";
-import Card from "react-bootstrap/Card";
-import ListGroup from "react-bootstrap/ListGroup";
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import { withAuth0 } from '@auth0/auth0-react';
+import axios from 'axios';
+import Card from 'react-bootstrap/Card';
+import ListGroup from 'react-bootstrap/ListGroup';
+import FormBooks from './FormBooks';
+
+
+
 
 class BestBooks extends React.Component {
+
   constructor(props) {
     super(props);
     this.state = {
       book: [],
-    };
+
+      name: '',
+      description: '',
+      status: ''
+
+    }
+
+
   }
+
   componentDidMount = async () => {
     const { user } = this.props.auth0;
     const myBooks = `${process.env.REACT_APP_SERVER_URL}/books?email=${user.email}`;
-    const showApiUrlBook = await axios.get(myBooks);
-    this.setState({ book: showApiUrlBook.data[0].books });
-  };
+    const showApiUrlbook = await axios.get(myBooks);
+    this.setState({ book: showApiUrlbook.data });
+  }
+
+  addBook = async (e) => {
+    e.preventDefault();
+
+    // sending the request to backend 
+    const bodyData = {
+      name: this.state.name,
+      status: this.state.status,
+      description: this.state.description,
+      email: this.props.auth0.user.email
+
+    }
+    const newBook = await axios.post(`${this.state.server}/book`, bodyData);
+
+
+    this.setState({
+      book: newBook.data
+    })
+  }
+
+
+
+  updateBookName = (e) => this.setState({ name: e.target.value });
+  updateDiscOfBook = (e) => this.setState({ description: e.target.value });
+  updateStatusOfBook = (e) => this.setState({ status: e.target.value });
+
+  addBook = async (e) => {
+    e.preventDefault();
+
+    const bodyData = {
+      name: this.state.name,
+      description: this.state.description,
+      status: this.state.status,
+      email: this.props.auth0.user.email
+
+    }
+
+    const newBook = await axios.post(`${process.env.REACT_APP_SERVER_URL}/books`, bodyData);
+
+    this.setState({
+      book: newBook.data
+    })
+  }
+
   render() {
+
     return (
-      <>
-        {this.state.book.length > 0 &&
-          this.state.book.map((element) => {
-            return (
-              <Card style={{ width: "18rem" }}>
-                <ListGroup variant="flush">
-                  <ListGroup.Item style={{ backgroundColor: "#845460", color: "#E2BCB7" }} as="li">
-                    book Name:
-                    {element.name}
-                  </ListGroup.Item>
-                  <ListGroup.Item>description: {element.description}</ListGroup.Item>
-                  <ListGroup.Item>status: {element.status}</ListGroup.Item>
-                </ListGroup>
-              </Card>
-            );
-          })}
-      </>
+     
+
+      <Jumbotron   >
+        <h1>My Favorite Books</h1>
+        <p>
+          This is a collection of my favorite books
+        </p>
+
+        <FormBooks
+          addBook={this.addBook}
+
+          updateBookName={this.updateBookName}
+          updateDiscOfBook={this.updateDiscOfBook}
+          updateStatusOfBook={this.updateStatusOfBook}
+        />
+
+        {
+          this.state.book.map(element => {
+            return <Card style={{ width: '18rem' }}>
+              <ListGroup variant="flush">
+                <ListGroup.Item as="li" active>book Name:
+                {element.name}</ListGroup.Item>
+                <ListGroup.Item>description: {element.description}</ListGroup.Item>
+                <ListGroup.Item>status: {element.status}</ListGroup.Item>
+              </ListGroup>  </Card>;
+          })
+        }
+
+
+      </Jumbotron >
+   
     );
+
   }
 }
+
 
 export default withAuth0(BestBooks);
